db2309788817142f082aae46f2d123e35f5b20e5
ceOSU <embree.14@osu.edu>
2024-06-26 18:26:10 -0400

    reformat script

--- copy_files.sh
+++ copy_files.sh
@@ -4,10 +4,10 @@ dir="/c/Users/Caleb/OneDrive - The Ohio State University/BioinfoData"
 for i in $(cat Kallisto_and_DEseq2/path_to_code.txt)
 do
 
-pwd
-echo $i
-cd "$dir"/"$i"
-cp PTC_analysis.R "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/Kallisto_and_DEseq2/""$i""_kallisto.R"
-cd "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/"
+    pwd
+    echo $i
+    cd "$dir"/"$i"
+    cp PTC_analysis.R "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/Kallisto_and_DEseq2/""$i""_kallisto.R"
+    cd "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/"
 
 done
