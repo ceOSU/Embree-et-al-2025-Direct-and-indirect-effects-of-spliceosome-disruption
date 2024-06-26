3fb2a9dae7579a5253983bdaf739f807f268ddd4
ceOSU <embree.14@osu.edu>
2024-06-26 18:22:59 -0400

    Initial test for copying files

---
+++ copy_files.sh
@@ -0,0 +1,13 @@
+cd "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/"
+pwd
+dir="/c/Users/Caleb/OneDrive - The Ohio State University/BioinfoData"
+for i in $(cat Kallisto_and_DEseq2/path_to_code.txt)
+do
+
+pwd
+echo $i
+cd "$dir"/"$i"
+cp PTC_analysis.R "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/Kallisto_and_DEseq2/""$i""_kallisto.R"
+cd "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/"
+
+done
