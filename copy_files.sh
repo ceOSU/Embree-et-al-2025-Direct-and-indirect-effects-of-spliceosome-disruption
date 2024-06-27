eccd1ba9c649c8e1ad560ed58efd9812c06e40f1
ceOSU <embree.14@osu.edu>
2024-06-27 13:08:36 -0400

    Update copy script for ISAR

--- copy_files.sh
+++ copy_files.sh
@@ -1,13 +1,29 @@
-cd "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/"
-pwd
+GR_dir="/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/"
 dir="/c/Users/Caleb/OneDrive - The Ohio State University/BioinfoData"
-for i in $(cat Kallisto_and_DEseq2/path_to_code.txt)
+cd $GR_dir
+pwd
+for i in $(cat Kallisto_and_DEseq2/path_to_kallisto.txt) #Copy all the kallisto/ DEseq R scripts to the github repository
 do
 
     pwd
-    echo $i
+    echo $i kallisto file copy
     cd "$dir"/"$i"
-    cp PTC_analysis.R "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/Kallisto_and_DEseq2/""$i""_kallisto.R"
-    cd "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/"
+    cp *.R "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/Kallisto_and_DEseq2/""$i""_kallisto.R"
+    cd $GR_dir
 
 done
+
+cp "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Figures/Data/NMD_TPM/TPM_graph.R" \
+    ./final_plots/final_plots.R #Copies the R file to make the final graphs
+
+for i in $(cat IsoformSwitchAnalyzeR/path_to_ISAR.text) #Copies the ISAR scripts to the github repository
+do
+    pwd
+    echo $i file copy
+    cd "$dir"/IsoformSwitch/"$i"
+    cp *.R "/c/Users/Caleb/OneDrive - The Ohio State University/Splicing and NMD/Git_repository/IsoformSwitchAnalyzeR/""$i""_script.R"
+    cd $GR_dir
+
+done
+
+cp "$dir"/IsoformSwitch/Combined_analysis/*.R ./IsoformSwitchAnalyzeR/fCombined_ISAR_analysis.R 
